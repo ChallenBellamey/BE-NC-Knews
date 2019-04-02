cec0019205681dd572b9227ed9f357e549ae3a71
Challen Bellamey <challenbellamey@hotmail.com>
2019-04-02 16:36:12 +0100

    CORS added to app.js

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const app = require('express')();
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const { apiRouter } = require('./routes/api-router');
 const { handle400, handle404, handle405, handle422, handle500 } = require('./errors')
 
+app.use(cors());
 app.use(bodyParser.json());
 
 app.use('/api', apiRouter);
