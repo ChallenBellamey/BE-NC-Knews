890badce57bcc04a0ef57152ab25a97c17656602
Challen Bellamey <challenbellamey@hotmail.com>
2019-06-12 22:28:21 +0100

    - Amend user data

--- db/data/test-data/users.js
+++ db/data/test-data/users.js
@@ -8,31 +8,31 @@ module.exports = [
   {
     username: 'grumpy19',
     name: 'Paul Grump',
-    about: 'Hi, my name is Tom Tickle',
+    about: 'Hi, my name is Paul Grump',
     last_online: 1500000000000
   },
   {
     username: 'happyamy2016',
     name: 'Amy Happy',
-    about: 'Hi, my name is Tom Tickle',
+    about: 'Hi, my name is Amy Happy',
     last_online: 1500000000000
   },
   {
     username: 'cooljmessy',
     name: 'Peter Messy',
-    about: 'Hi, my name is Tom Tickle',
+    about: 'Hi, my name is Peter Messy',
     last_online: 1500000000000
   },
   {
     username: 'weegembump',
     name: 'Gemma Bump',
-    about: 'Hi, my name is Tom Tickle',
+    about: 'Hi, my name is Gemma Bump',
     last_online: 1500000000000
   },
   {
     username: 'jessjelly',
     name: 'Jess Jelly',
-    about: 'Hi, my name is Tom Tickle',
+    about: 'Hi, my name is Jess Jelly',
     last_online: 1500000000000
   },
 ];
